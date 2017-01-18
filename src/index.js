69fedc764727a0852eee4df5785ce6f97c111ece
hitosaka21 <osaka708@gmail.com>
2017-01-18 21:16:23 +0900

    axios

--- src/index.js
+++ src/index.js
@@ -33,11 +33,19 @@ class TodoInput extends React.Component {
 		this.props.onAddToDo(newTodo);
 		this.refs.todo.value="";
 	}
+	_getOnsen(){
+    console.log("Onsen");
+    axios.post('localhost:8080/onsen/list')
+      .then(function (res) {
+        console.log(res.data);
+      });
+	}
 	render () {
 		return (
 			<div>
 				<input type="text" ref="todo" />
 				<input type="button" value="submit" onClick={this._onAddToDo.bind(this)} />
+				<input type="button" value="温泉を取得" onClick={this._getOnsen.bind(this)} />
 			</div>
 		)
 	}
