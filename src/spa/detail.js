d5025f7391e5a884021f419ddc35ad0abbd079ae
d.kanai <d.kanai@pastoraldog.com>
2017-01-25 20:58:33 +0900

    温泉詳細画面追加

---
+++ src/spa/detail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Component } from 'react'
+
+class SpaDetail extends Component {
+	render () {
+		return (
+			<div>
+				木下温泉詳細！！！！
+			</div>
+		)
+	}
+}
+
+export default SpaDetail
