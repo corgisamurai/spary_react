d7f75c20ad7bbe09c2549f1cbd7b6efdd6d7a3e3
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-03-29 22:00:53 +0900

    add new column in spa detail

--- src/spa/detail.js
+++ src/spa/detail.js
@@ -24,11 +24,22 @@ class SpaDetail extends Component {
         <dl>
           <dt>温泉名</dt>
           <dd>{ this.state.spa.name }</dd>
+          <dt>写真イメージ</dt>
+          <dd><img src={`${this.state.spa.image}` } /></dd>
+          <dt>営業時間</dt>
+          <dd>{ this.state.spa.workday }</dd>
+          <dt>料金</dt>
+          <dd>{ this.state.spa.fee }</dd>
+          <dt>効能</dt>
+          <dd>{ this.state.spa.effect }</dd>
+          <dt>設備</dt>
+          <dd>{ this.state.spa.equipment }</dd>
           <dt>住所</dt>
           <dd>{ this.state.spa.address }</dd>
           <dt>ホームページ</dt>
           <dd>{ this.state.spa.url }</dd>
-
+          <dt>電話番号</dt>
+          <dd>{ this.state.spa.tel }</dd>
         </dl>
 			</div>
 		)
