d7721c22b49a4d656623cf05676117b64ba56b7c
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-03-29 21:41:42 +0900

    use spa api, it can get spa detail when chose spa

--- src/spa/detail.js
+++ src/spa/detail.js
@@ -2,10 +2,34 @@ import React from 'react'
 import { Component } from 'react'
 
 class SpaDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { spa: [] }
+  }
+  componentDidMount () {
+    this.getSpa()
+  }
+  getSpa () {
+    axios
+    .get('/v1/spa/' + this.props.params.id)
+    .then((res) => {
+      console.log(res.data)
+      this.setState({spa: res.data})
+    })
+  }
+
 	render () {
 		return (
 			<div>
-				木下温泉詳細！！！！
+        <dl>
+          <dt>温泉名</dt>
+          <dd>{ this.state.spa.name }</dd>
+          <dt>住所</dt>
+          <dd>{ this.state.spa.address }</dd>
+          <dt>ホームページ</dt>
+          <dd>{ this.state.spa.url }</dd>
+
+        </dl>
 			</div>
 		)
 	}
