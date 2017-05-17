e6a88743b1d8e578b5e4be118ca486a84b61e647
横尾　千明 <vv001292@CA2332.local>
2017-05-17 21:56:00 +0900

    submitボタン押下とpasswordの取得まで

--- src/spa/login.js
+++ src/spa/login.js
@@ -4,6 +4,17 @@ import { Link } from 'react-router';
 
 
 class SpaLogin extends Component {
+  _onSubmit(){
+    var id = this.refs.id.value.trim();
+    var password = this.refs.password.value.trim();
+    //TODO:go側と繋げる
+    // axios.post('/v1/login',{
+    //   userName:id,
+    //   password:password
+    // }).then(function (res) {
+    //     console.log(res.data);
+    //   });
+  }
 	render () {
 		return (
 			<div>
@@ -12,9 +23,9 @@ class SpaLogin extends Component {
         <input type="text" ref="id" />
         <label>password:</label>
         <input type="password" ref="password" />
-        <Link to="/spa">
-          submit
-        </Link>
+        <Link to="/spa" />
+        <input type="button" value="submit" onClick={this._onSubmit.bind(this)}/>
+
 		  </div>
 		)
   }
